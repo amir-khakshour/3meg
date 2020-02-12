8c4d50a4eda78942d0d892ebabd1e5937675266e
amir-khakshour <khakshour.amir@gmail.com>
2020-02-12 18:23:53 +0100

    - TDD Cycle - Red - Create tests for datapoints API

---
+++ src/tests/functional/plant/test_datapoint_api.py
@@ -0,0 +1,14 @@
+from tests.utils import APITest
+from tests.factories import DataPointFactory
+
+
+class DataPointAPITest(APITest):
+    def setUp(self):
+        super().setUp()
+        self.num_datapoints = 9
+        self.datapoint_list = [DataPointFactory() for _ in range(self.num_datapoints)]
+
+    def test_datapoint_list(self):
+        self.response = self.get("api_plant:datapoint-list")
+        self.response.assertStatusEqual(200)
+        self.assertEqual(len(self.response.body), self.datapoint_list)
