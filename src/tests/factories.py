b6b6bcf86e7aa84fa9079bf368e07917911ea52e
amir-khakshour <khakshour.amir@gmail.com>
2020-02-12 17:17:20 +0100

    - Added tests folder structure - Added some utility function and classes for testing including model factories for Plant and DataPoint models

---
+++ src/tests/factories.py
@@ -0,0 +1,35 @@
+import datetime
+import factory
+from factory import fuzzy
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+from plant.models import Plant, DataPoint
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = get_user_model()
+
+    first_name = factory.Sequence(lambda n: "User %03d" % n)
+    username = factory.Sequence(lambda n: "user_%d" % n)
+    email = factory.Sequence(lambda n: "user_%d@example.com" % n)
+
+
+class PlantFactory(factory.DjangoModelFactory):
+    name = factory.Sequence(lambda n: 'Plant %d' % n)
+
+    class Meta:
+        model = Plant
+
+
+class DataPointFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = DataPoint
+
+    plant = factory.SubFactory(PlantFactory)
+    energy_expected = factory.fuzzy.FuzzyDecimal(0.1111111, 80.11111111)
+    energy_observed = factory.fuzzy.FuzzyDecimal(0.1111111, 80.11111111)
+    irradiation_expected = factory.fuzzy.FuzzyDecimal(0.1111111, 80.11111111)
+    irradiation_observed = factory.fuzzy.FuzzyDecimal(0.1111111, 80.11111111)
+    datetime = factory.fuzzy.FuzzyDateTime(timezone.now() - datetime.timedelta(days=10))
---
+++ src/tests/utils.py
@@ -0,0 +1,179 @@
+import json
+from re import match
+from importlib import reload as reload_module
+
+from django.http import SimpleCookie
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse, NoReverseMatch
+
+User = get_user_model()
+
+
+class APITest(TestCase):
+    longMessage = True
+
+    def setUp(self):
+        # Create admin user
+        user = User.objects.create_user(
+            username="admin", email="admin@3megawat.local", password="admin"
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        self.admin_user = user
+
+        # Create normal staff user
+        user = User.objects.create_user(
+            username="nobody", email="nobody@nobody.local", password="nobody"
+        )
+        user.is_staff = True
+        user.is_superuser = False
+        user.save()
+        self.staff_user = user
+
+        # Create another normal user
+        user = User.objects.create_user(
+            username="somebody", email="somebody@nobody.local", password="somebody"
+        )
+        user.is_staff = False
+        user.is_superuser = False
+        user.save()
+        self.normal_user = user
+
+    def login(self, username, password):
+        result = self.client.login(username=username, password=password)
+        self.assertTrue(result, "%s should be able to log in" % username)
+        return True
+
+    def hlogin(self, username, password, session_id):
+        response = self.post(
+            "api-login", session_id, username=username, password=password
+        )
+        self.assertEqual(
+            response.status_code,
+            200,
+            "%s should be able to login via the api" % username,
+        )
+        return True
+
+    def api_call(self, url_name, method, session_id=None,
+                 authenticated=False, version=None, url_kwargs=None, data=None):
+        if url_kwargs is None:
+            url_kwargs = {}
+        if version is None:
+            version = 1
+            url_kwargs.update({'version': version})
+        try:
+            url = reverse(url_name, kwargs=url_kwargs)
+        except NoReverseMatch:
+            url = url_name
+        method = getattr(self.client, method.lower())
+        kwargs = {"content_type": "application/json"}
+        if session_id is not None:
+            auth_type = "AUTH" if authenticated else "ANON"
+            kwargs["HTTP_SESSION_ID"] = "SID:%s:testserver:%s" % (auth_type, session_id)
+
+        if data:
+            response = method(url, json.dumps(data), **kwargs)
+        else:
+            response = method(url, **kwargs)
+        # throw away cookies when using session_id authentication
+        if session_id is not None:
+            self.client.cookies = SimpleCookie()
+
+        return response
+
+    def get(self, url_name, session_id=None, authenticated=False, version=None, url_kwargs=None):
+        return self.api_call(
+            url_name, "GET", session_id=session_id, authenticated=authenticated, version=version, url_kwargs=url_kwargs
+        )
+
+    def post(self, url_name, session_id=None, authenticated=False, version=None, url_kwargs=None, data=None):
+        return self.api_call(
+            url_name, "POST", session_id=session_id, authenticated=authenticated,
+            version=version, url_kwargs=url_kwargs, data=data
+        )
+
+    def put(self, url_name, session_id=None, authenticated=False, url_kwargs=None, data=None):
+        return self.api_call(
+            url_name, "PUT", session_id=session_id, authenticated=authenticated, url_kwargs=url_kwargs, data=data
+        )
+
+    def patch(self, url_name, session_id=None, authenticated=False, url_kwargs=None, data=None):
+        return self.api_call(
+            url_name,
+            "PATCH",
+            session_id=session_id,
+            authenticated=authenticated,
+            url_kwargs=url_kwargs,
+            data=data
+        )
+
+    def delete(self, url_name, session_id=None, url_kwargs=None, authenticated=False):
+        return self.api_call(
+            url_name, "DELETE", session_id=session_id, authenticated=authenticated, url_kwargs=url_kwargs
+        )
+
+    def tearDown(self):
+        User.objects.get(username="admin").delete()
+        User.objects.get(username="nobody").delete()
+
+    @property
+    def response(self):
+        return self._response
+
+    @response.setter
+    def response(self, response):
+        self._response = ParsedResponse(response, self)
+
+    @staticmethod
+    def reload_modules(modules=()):
+        for module in modules:
+            reload_module(module)
+
+
+class ParsedResponse(object):
+    def __init__(self, response, unittestcase):
+        self.response = response
+        self.t = unittestcase
+
+    @property
+    def response(self):
+        return self._response
+
+    @response.setter
+    def response(self, response):
+        self._response = response
+        self.status_code = response.status_code
+        try:
+            self.body = response.data
+        except Exception:
+            self.body = None
+
+    def __getattr__(self, name):
+        return getattr(self._response, name)
+
+    def __getitem__(self, name):
+        return self.body[name]
+
+    def __len__(self):
+        return len(self.body)
+
+    def assertStatusEqual(self, code, message=None):
+        self.t.assertEqual(self.status_code, code, message)
+
+    def assertValueEqual(self, value_name, value, message=None):
+        self.t.assertEqual(self[value_name], value, message)
+
+    def assertObjectIdEqual(self, value_name, value, message=None):
+        pattern = r".*?%s.*?/(?P<object_id>\d+)/?" % reverse("api-root")
+        m = match(pattern, self[value_name])
+        if m:
+            object_id = int(m.groupdict()["object_id"])
+        else:
+            object_id = None
+        self.t.assertEqual(object_id, value, message)
+
+    def __str__(self):
+        return str(self._response)
