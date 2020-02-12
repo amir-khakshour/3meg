00a105a64df132865080814ffafafab2903f042d
amir-khakshour <khakshour.amir@gmail.com>
2020-02-12 18:34:04 +0100

    - TDD cycle - Red - Added a test for plant endpoint listing datapoints in the given plant

--- src/tests/functional/plant/test_plant_api.py
+++ src/tests/functional/plant/test_plant_api.py
@@ -12,3 +12,8 @@ class PlantAPITest(APITest):
         self.response = self.get("api_plant:plant-list")
         self.response.assertStatusEqual(200)
         self.assertEqual(len(self.response.body), self.num_plants)
+
+    def test_datapoints_in_plant(self):
+        base_plant = self.plants_list[0]
+        self.response = self.get("api_plant:plant-datapoints", url_kwargs={'pk': base_plant.pk})
+        self.response.assertStatusEqual(200)
