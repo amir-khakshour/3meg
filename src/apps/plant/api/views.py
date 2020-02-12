0b22b63604b710bbb93f00a01c3a5fd69605a48a
amir-khakshour <khakshour.amir@gmail.com>
2020-02-12 18:44:03 +0100

    - TDD cycle - Green -  Make the tests for plant API endpoint listing datapoints in the given plant pass - Added an action endpoint to plant API listing datapoints in the given plant - Added some utility function for overriding temporarily the main serializers in a view

--- src/apps/plant/api/views.py
+++ src/apps/plant/api/views.py
@@ -1,12 +1,27 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from ..models import Plant, DataPoint
+from ..drf.utils import override_serializer
 from .serializers import PlantSerializer, DataPointSerializer
 
 
 class PlantViewSet(viewsets.ModelViewSet):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
+
+    @action(detail=True, methods=['get'])
+    def datapoints(self, request, pk, version=1):
+        products = DataPoint.objects.filter(plant_id=pk)
+        page = self.paginate_queryset(products)
+        with override_serializer(self, DataPointSerializer):
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+
+            serializer = self.get_serializer(products, many=True)
+        return Response(serializer.data)
 
 
 class DataPointViewSet(viewsets.ModelViewSet):
