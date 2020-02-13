0a9a64c87b4f99de02766ceda3f96fe736b89bcc
amir-khakshour <khakshour.amir@gmail.com>
2020-02-13 10:16:32 +0100

    - TDD cycle - Green - added a filter by date functionality to DataPoints API endpoint

--- src/apps/plant/api/filters.py
+++ src/apps/plant/api/filters.py
@@ -4,6 +4,14 @@ from ..models import DataPoint
 
 
 class DataPointFilter(django_filters.FilterSet):
+    """Filter for DataPoints by date"""
+    date = django_filters.DateFilter(field_name='datetime', method='filter_date')
+
+    def filter_date(self, queryset, name, value):
+        # construct the full lookup expression.
+        lookup = '__'.join([name, 'date'])
+        return queryset.filter(**{lookup: value})
+
     class Meta:
         model = DataPoint
         fields = {
